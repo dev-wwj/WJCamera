5da72f15c648d6610fd77e292ec02f9db506d595
wangwenjian1 <wangwenjian1@021.com>
2021-03-01 18:31:26 +0800

    fix bug

--- Example/Pods/Target Support Files/WJCamera/WJCamera-umbrella.h
+++ Example/Pods/Target Support Files/WJCamera/WJCamera-umbrella.h
@@ -13,12 +13,10 @@
 #import "CaptureView.h"
 #import "HAVPlayer.h"
 #import "NSBundle+WJLibrary.h"
-#import "SavaFileALbumUtil.h"
+#import "UIButton+Enlarge.h"
 #import "UIImage+WJLibrary.h"
-#import "WJCameraConfig.h"
 #import "WJCameraController.h"
 #import "WJProgressView.h"
-#import "WJShootButton.h"
 #import "WJUtilDefine.h"
 
 FOUNDATION_EXPORT double WJCameraVersionNumber;
